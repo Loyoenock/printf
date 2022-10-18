6edbde8ce0d901a9fb5c89c322e0ed3130873e4d
Loyoenock <loyoenock@gmail.com>
2022-10-17 18:20:30 -0700

    get precision

--- get_precision.c
+++ get_precision.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_precision - Calculates printing precision
+ * @format: Formatted string to print arguments
+ * @i: Arguments to be printed.
  * @list: list of arguments.
  *
  * Return: Precision.
