aa2f3fff3433f5f702aa9c46aec13da62db771bc
Loyoenock <loyoenock@gmail.com>
2022-10-17 18:30:43 -0700

    utils

--- utils.c
+++ utils.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * is_printable - Evaluates if a character is printable
+ * @c: Character to be evaluated.
  *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 if c is printable, else 0
  */
 int is_printable(char c)
 {
@@ -17,7 +17,7 @@ int is_printable(char c)
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
- * @i: Index at which to start appending.
+ * @i: Index to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
